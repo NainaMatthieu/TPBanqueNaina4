d871445bc8d622c329de64e80d4692e8ea3c8cce
NainaMatthieu <andrianmattax@gmail.com>
2024-04-12 20:03:08 +0300

    Configuration de JSF et création des templates

---
+++ src/main/java/mg/itu/tpbanquenaina4/config/ConfigJSF.java
@@ -0,0 +1,14 @@
+package mg.itu.tpbanquenaina4.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ * Configuration JSF
+ *classe CDI pour activer
+ * @author NainaMatthieu
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
