de5a3158be4f30a63d825afdf75c6c396d1b7d22
NainaMatthieu <andrianmattax@gmail.com>
2024-04-13 23:04:30 +0300

    Déposer ou retirer de l'argent sur un compte

--- src/main/java/mg/itu/tpbanquenaina4/service/GestionnaireCompte.java
+++ src/main/java/mg/itu/tpbanquenaina4/service/GestionnaireCompte.java
@@ -52,14 +52,36 @@ public class GestionnaireCompte implements Serializable {
     @Transactional
     public void transferer(CompteBancaire source, CompteBancaire destination,
             int montant) {
-        source.retirer(montant);
-        destination.deposer(montant);
-        update(source);
-        update(destination);
+        retirer(source, montant);
+        deposer(destination, montant);
     }
 
     @Transactional
     public CompteBancaire update(CompteBancaire compteBancaire) {
         return em.merge(compteBancaire);
+    }
+
+    /**
+     * Dépôt d'argent sur un compte bancaire.
+     *
+     * @param compteBancaire
+     * @param montant
+     */
+    @Transactional
+    public void deposer(CompteBancaire compteBancaire, int montant) {
+        compteBancaire.deposer(montant);
+        update(compteBancaire);
+    }
+
+    /**
+     * Retrait d'argent sur un compte bancaire.
+     *
+     * @param compteBancaire
+     * @param montant
+     */
+    @Transactional
+    public void retirer(CompteBancaire compteBancaire, int montant) {
+        compteBancaire.retirer(montant);
+        update(compteBancaire);
     }
 }
