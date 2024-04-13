b5e1e8bf78353e222f218f721dd2be95906278a3
NainaMatthieu <andrianmattax@gmail.com>
2024-04-13 21:51:42 +0300

    Gestion des erreurs et des messages de success

--- src/main/java/mg/itu/tpbanquenaina4/jsf/TransfertArgent.java
+++ src/main/java/mg/itu/tpbanquenaina4/jsf/TransfertArgent.java
@@ -4,6 +4,7 @@ import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import mg.itu.tpbanquenaina4.entity.CompteBancaire;
+import mg.itu.tpbanquenaina4.jsf.util.Util;
 import mg.itu.tpbanquenaina4.service.GestionnaireCompte;
 
 /**
@@ -54,9 +55,32 @@ public class TransfertArgent {
     }
 
     public String transferer() {
+        boolean erreur = false;
         CompteBancaire comptebancaireSource = gestionnaireCompte.findById(idCompteSource);
+        if (comptebancaireSource == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            erreur = true;
+        } else {
+            if (comptebancaireSource.getSolde() < montant) {
+                 Util.messageErreur("Le solde de votre compte est insuffisant", "Solde insuffisant!", "form:montant");
+                erreur = true;
+            }
+        }
+         if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
         CompteBancaire comptebancaireTarget = gestionnaireCompte.findById(idCompteTarget);
+        if(comptebancaireTarget==null){
+             Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destination");
+             return null;
+        }
         gestionnaireCompte.transferer(comptebancaireSource, comptebancaireTarget, montant);
+         String message = String.format("Transfert de %s effectué avec succès du compte de %s vers le compte de %s.",
+                                         montant, comptebancaireSource.getNom(), comptebancaireTarget.getNom());
+         Util.addFlashInfoMessage(message);
         return "listeComptes?faces-redirect=true";
     }
 }
