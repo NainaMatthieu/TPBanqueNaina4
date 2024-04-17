ba3401df46e9957eda38c9869d71eb78ff49f16c
NainaMatthieu <andrianmattax@gmail.com>
2024-04-17 14:47:49 +0300

    Creation d'une classe entité OperationBancaire

---
+++ src/main/java/mg/itu/tpbanquenaina4/entity/OperationBancaire.java
@@ -0,0 +1,93 @@
+package mg.itu.tpbanquenaina4.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Classe représentant une opération bancaire.
+ *
+ * Historique pour suivre les modifications effectuées sur les comptes.
+ *
+ * Il est caractérisé par un déscription de l'opération faite sur le compte, la
+ * date de l'opération le montant ( dépôt, retrait d'argent).
+ *
+ * @author NainaMatthieu
+ */
+@Entity
+public class OperationBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descritpion;
+    private LocalDateTime dateOperation;
+    private int montant;
+
+    public OperationBancaire() {
+    }
+
+    public OperationBancaire(String descritpion, LocalDateTime dateOperation, int montant) {
+        this.descritpion = descritpion;
+        this.dateOperation = LocalDateTime.now();
+        this.montant = montant;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescritpion() {
+        return descritpion;
+    }
+
+    public void setDescritpion(String descritpion) {
+        this.descritpion = descritpion;
+    }
+
+    public LocalDateTime getDateOperation() {
+        return dateOperation;
+    }
+
+    public void setDateOperation(LocalDateTime dateOperation) {
+        this.dateOperation = dateOperation;
+    }
+
+    public int getMontant() {
+        return montant;
+    }
+
+    public void setMontant(int montant) {
+        this.montant = montant;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof OperationBancaire)) {
+            return false;
+        }
+        OperationBancaire other = (OperationBancaire) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mg.itu.tpbanquenaina4.entity.OperationBancaire[ id=" + id + " ]";
+    }
+
+}
